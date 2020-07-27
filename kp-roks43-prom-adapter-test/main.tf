fcd78680570dbb43ae0b2fa9c82afc09802e296f
jpapejr <jpapejr@icloud.com>
2020-07-27 11:05:49 -0400

    odd stuff..

--- kp-roks43-prom-adapter-test/main.tf
+++ kp-roks43-prom-adapter-test/main.tf
@@ -7,6 +7,7 @@ resource "ibm_network_vlan" "cluster_vlan_public" {
   name       = "kp-roks43-prom-ad-pb"
   datacenter = "wdc07"
   type       = "PUBLIC"
+  tags       = ["kp", "cmo", "prom-adapter"]
 }
 
 # Create a private vlan
@@ -14,15 +15,16 @@ resource "ibm_network_vlan" "cluster_vlan_private" {
   name       = "kp-roks43-prom-ad-pr"
   datacenter = "wdc07"
   type       = "PRIVATE"
+  tags       = ["kp", "cmo", "prom-adapter"]
 }
 
 resource "ibm_subnet" "portable_subnet" {
   type = "Portable"
-  private = "false"
+  private = false
   ip_version = 4
   capacity = 16
-  vlan_id = data.ibm_network_vlan.cluster_vlan_public.id
-  notes = "kp-roks43-prom-adapt-portable"
+  vlan_id = ibm_network_vlan.cluster_vlan_public.id
+  notes = "kp-roks43-prom-adapter-test"
   //User can increase timeouts 
   timeouts {
     create = "45m"
@@ -34,13 +36,13 @@ resource "ibm_container_cluster" "cluster" {
   name                     = "kp-roks43-prom-adapter-test"
   datacenter               = "wdc07"
   no_subnet                = true
-  subnet_id                = data.ibm_subnet.portable_subnet.id
+  subnet_id                = ibm_subnet.portable_subnet.id
   default_pool_size        = 3
   hardware                 = "shared"
   resource_group_id        = data.ibm_resource_group.default_resource_group.id
   machine_type             = "b3c.8x32"
-  public_vlan_id           = data.ibm_network_vlan.cluster_vlan_public.id
-  private_vlan_id          = data.ibm_network_vlan.cluster_vlan_private.id
+  public_vlan_id           = ibm_network_vlan.cluster_vlan_public.id
+  private_vlan_id          = ibm_network_vlan.cluster_vlan_private.id
   public_service_endpoint  = true
   private_service_endpoint = true
   tags                     = ["kp", "cmo", "prom-adapter"]
