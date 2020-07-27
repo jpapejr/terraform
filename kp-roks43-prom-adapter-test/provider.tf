17dc3aa1e75d4482760f04510ab768e75f1d8603
jpapejr <jpapejr@icloud.com>
2020-07-27 10:24:14 -0400

    add cred vars

--- kp-roks43-prom-adapter-test/provider.tf
+++ kp-roks43-prom-adapter-test/provider.tf
@@ -1,3 +1,6 @@
+varible "iaas_classic_username" {}
+varible "iaas_classic_api_key" {}
+
 provider "ibm" {
     iaas_classic_username = var.iaas_classic_username
     iaas_classic_api_key  = var.iaas_classic_api_key
