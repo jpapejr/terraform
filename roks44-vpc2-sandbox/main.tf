181e57db9ab06bda2fa6fd75c168abe6d0024e20
jpapejr <jpapejr@icloud.com>
2020-07-28 17:56:03 -0400

    adding rg back in for id

--- roks44-vpc2-sandbox/main.tf
+++ roks44-vpc2-sandbox/main.tf
@@ -1,3 +1,6 @@
+data "ibm_resource_group" "default_resource_group" {
+  name = "default"
+}
 
 locals  {
   tags       =  ["sandbox"]
@@ -45,7 +48,7 @@ resource "ibm_container_vpc_cluster" "cluster" {
   flavor            = "bx2.4x16"
   worker_count      = "3"
   cos_instance_crn  = ibm_resource_instance.cos.id
-  resource_group_id = local.res_group
+  resource_group_id = data.ibm_resource_group.default_resource_group.id
   zones = [
       {
          subnet_id = ibm_is_subnet.subnet1.id
