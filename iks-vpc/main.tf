826b4e974d3b0349b8d8491589fea9fac92ebca4
jpapejr <jpapejr@icloud.com>
2020-10-16 13:23:24 -0400

    add api key var for local tf runs

--- iks-vpc/main.tf
+++ iks-vpc/main.tf
@@ -1,4 +1,9 @@
+# Not required for IBM Cloud Schematics but required
+# for local Terraform runs
+variable ibmcloud_api_key {}
+
 provider "ibm" {
-  generation = 2
-  region     = var.region
+  ibmcloud_api_key = var.ibmcloud_api_key
+  generation       = 2
+  region           = var.region
 }
