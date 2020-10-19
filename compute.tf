c61dbe79426665f4b585b8a2abe165b60340a16a
jpapejr <jpapejr@icloud.com>
2020-10-19 13:14:33 -0400

    more typos from merge

--- compute.tf
+++ compute.tf
@@ -4,14 +4,14 @@ resource "ibm_is_instance" "instance1" {
   profile    = var.profile
 
   primary_network_interface {
-    subnet   = ibm_is_subnet.iac_subnet.id
-    security_groups = [ ibm_is_vpc.iac_vpc.default_security_group ]
+    subnet   = ibm_is_vpc.iac_iks_vpc.id
+    security_groups = [ ibm_is_vpc.iac_iks_vpc.default_security_group ]
   }
 
-  vpc            = ibm_is_vpc.iac_vpc.id
+  vpc            = ibm_is_vpc.iac_iks_vpc.id
   zone           = var.zone
   resource_group = ibm_resource_group.group.id
   keys           = [data.ibm_is_ssh_key.sshkey.id]
   user_data      = file("${path.module}/bootstrap_v2.sh")
-  depends_on     = [ ibm_resource_group.group, ibm_is_vpc.iac_vpc, ibm_is_subnet.iac_subnet ]
+  depends_on     = [ ibm_resource_group.group, ibm_is_vpc.iac_iks_vpc, ibm_is_subnet.iac_subnet ]
 }
