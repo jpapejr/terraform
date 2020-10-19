d1f12d274ff5fdd76b2191b8037e5c0615b72fbc
jpapejr <jpapejr@icloud.com>
2020-10-19 10:41:56 -0400

    re-org

--- vpc-compute/compute.tf
+++ vpc-compute/compute.tf
@@ -1,14 +1,16 @@
 resource "ibm_is_instance" "instance1" {
-  name    = var.name
-  image   = var.imageid
-  profile = var.profile
+  name       = "${var.project_name}-${var.environment}-${var.instance_identifier}"
+  image      = var.imageid
+  profile    = var.profile
 
   primary_network_interface {
-    subnet = var.subnetid
-    security_groups = [ var.sgid ]
+    subnet   = ibm_is_subnet.iac_subnet.id
+    security_groups = [ ibm_is_vpc.iac_vpc.default_security_group ]
   }
 
-  vpc       = data.ibm_is_vpc.vpc.id
-  zone      = var.zone
-  keys      = [data.ibm_is_ssh_key.sshkey.id]
+  vpc        = ibm_is_vpc.iac_vpc.id
+  zone       = var.zone
+  keys       = [data.ibm_is_ssh_key.sshkey.id]
+  user_data  = file("${path.module}/bootstrap_v2.sh")
+  depends_on = [ ibm_resource_group.group, ibm_is_vpc.iac_vpc, ibm_is_subnet.iac_subnet ]
 }
--- vpc-compute/network.tf
+++ vpc-compute/network.tf
@@ -1,9 +1,44 @@
-data "ibm_is_vpc" "vpc" {
-  name = var.vpc
+resource "ibm_is_vpc" "iac_vpc" {
+  name                      = "${var.project_name}-${var.environment}-vpc"
+  resource_group            = ibm_resource_group.group.id
+  address_prefix_management = "manual"
+  depends_on                = [ ibm_resource_group.group ]
+}
+
+resource "ibm_is_vpc_address_prefix" "vpc_address_prefix" {
+  name                      = "${var.project_name}-${var.environment}-range-00"
+  zone                      = var.zone
+  vpc                       = ibm_is_vpc.iac_vpc.id
+  cidr                      = "10.240.0.0/24"
+  depends_on                = [ ibm_is_vpc.iac_vpc ]
+}
+
+resource "ibm_is_security_group_rule" "iac_iks_security_group_rule_tcp_k8s" {
+  group     = ibm_is_vpc.iac_vpc.default_security_group
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+
+  tcp {
+    port_min = 22
+    port_max = 22
+  }
+}
+
+resource "ibm_is_subnet" "iac_subnet" {
+  name                      = "${var.project_name}-${var.environment}-subnet-00"
+  zone                      = var.zone
+  vpc                       = ibm_is_vpc.iac_vpc.id
+  ipv4_cidr_block           = "10.240.0.0/26"
+  
+  # total_ipv4_address_count = 64
+  resource_group            = ibm_resource_group.group.id
+  
+  depends_on                = [ibm_is_vpc_address_prefix.vpc_address_prefix]
 }
 
 resource "ibm_is_floating_ip" "floatingip1" {
-  name   = "${var.name}-ip"
-  target = ibm_is_instance.instance1.primary_network_interface[0].id
+  name                      = "${var.project_name}-${var.environment}-fip"
+  target                    = ibm_is_instance.instance1.primary_network_interface[0].id
+  depends_on                = [ ibm_is_vpc.iac_vpc ]
 }
 
